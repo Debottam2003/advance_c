cfd65e4f548877ee6617f95beba6c6414c26e4e0
Debottam Kar <debottamkar2003@gmail.com>
2026-08-05 22:58:17 +0530

    Let's ace it

---
+++ second.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+int f = 80;
+int c = 0;
+// c = (5.0 / 9.0) * (f - 32);// implicit type conversion
+c = (int) ((5.0 / 9.0) * (f - 32));// explicit type conversion
+printf("%d\n",c);
+}
---
+++ start.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main() {
+    printf("Hello, World!\n");
+    printf("Hello, ");
+    printf("World!");
+    printf("\n");
+    // print("\c");
+    // warning: unknown escape sequence: ‘\c’
+    printf("\\c");
+}
