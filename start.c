6f9e8f8ec3f8eef5fc59acb1ce3558c2a237545b
Debottam Kar <debottamkar2003@gmail.com>
2026-09-25 18:09:41 +0530

    fixes

--- start.c
+++ start.c
@@ -8,4 +8,5 @@ int main() {
     // printf("\c");
     // warning: unknown escape sequence: ‘\c’
     printf("\\c\n");
+    return 0; // New version of C returns 0 by default and 0 means success. If you return 1, it means failure.
 }
